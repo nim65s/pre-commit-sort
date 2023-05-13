9d9ae1cc6df4153ccb431d0c324c3814120d1eb2
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-05-14 01:49:49 +0200

    test deserialize

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+/*
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, Write};
+use std::path::Path;
+
+use pre_commit_config_sort::PreCommitConfig;
+*/
+
+fn main() -> anyhow::Result<()> {
+    let _path = ".pre-commit-config.yaml";
+
+    /*
+    if Path::new(path).exists() {
+        println!("file exist");
+        let input = File::open(path)?;
+        //let mut data: PreCommitConfig = serde_yaml::from_reader(input)?;
+        data.sort();
+        let input = File::open(path)?;
+        serde_yaml::to_writer(input, &data)?;
+    }
+    */
+    Ok(())
 }
--- tests/test_default.rs
+++ tests/test_default.rs
@@ -2,7 +2,7 @@ use indoc::indoc;
 use pre_commit_config_sort::{Hook, PreCommitConfig, Repo};
 
 #[test]
-fn test_example() {
+fn test_serialize() {
     let mut example = PreCommitConfig::new();
 
     let mut pre_commit = Repo::new("https://github.com/pre-commit/pre-commit-hooks", "v2.3.0");
@@ -61,4 +61,34 @@ fn test_sort() {
     assert_ne!(serde_yaml::to_string(&example).unwrap(), yaml);
     example.sort();
     assert_eq!(serde_yaml::to_string(&example).unwrap(), yaml);
+}
+
+#[test]
+fn test_deserialize() {
+    let mut example = PreCommitConfig::new();
+
+    let mut pre_commit = Repo::new("https://github.com/pre-commit/pre-commit-hooks", "v2.3.0");
+    for hook in ["check-yaml", "end-of-file-fixer", "trailing-whitespaces"] {
+        pre_commit.add_hook(Hook::new(hook));
+    }
+    example.add_repo(pre_commit);
+
+    let mut black = Repo::new("https://github.com/psf/black", "22.10.0");
+    black.add_hook(Hook::new("black"));
+    example.add_repo(black);
+
+    let yaml = indoc! {"
+        repos:
+        - repo: https://github.com/pre-commit/pre-commit-hooks
+          rev: v2.3.0
+          hooks:
+          - id: check-yaml
+          - id: end-of-file-fixer
+          - id: trailing-whitespaces
+        - repo: https://github.com/psf/black
+          rev: 22.10.0
+          hooks:
+          - id: black
+        "};
+    assert_eq!(example, serde_yaml::from_str(yaml).unwrap());
 }
