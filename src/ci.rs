83a7dcf9c4b50a9e50d9d7065fd3b58e49a476ed
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-04-23 09:31:30 +0200

    validate ci autoupdate_schedule

--- src/ci.rs
+++ src/ci.rs
@@ -1,5 +1,14 @@
 //! ref. <https://pre-commit.ci/#configuration>
 
+#[allow(non_camel_case_types)]
+#[serde_with::skip_serializing_none]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Eq, Ord, PartialEq, PartialOrd, Clone)]
+pub enum CIAutoupdateSchedule {
+    weekly,
+    monthly,
+    quarterly,
+}
+
 #[serde_with::skip_serializing_none]
 #[derive(serde::Serialize, serde::Deserialize, Debug, Eq, Ord, PartialEq, PartialOrd, Clone)]
 pub struct CI {
@@ -7,7 +16,7 @@ pub struct CI {
     autofix_prs: Option<bool>,
     autoupdate_branch: Option<String>,
     autoupdate_commit_msg: Option<String>,
-    autoupdate_schedule: Option<String>,
+    autoupdate_schedule: Option<CIAutoupdateSchedule>,
     skip: Option<Vec<String>>,
     submodules: Option<bool>,
 }
