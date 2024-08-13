620f645eee59065b2a91f690041005627d4d4126
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-08-13 12:25:43 +0200

    flakify

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,8 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 <!-- next-header -->
 ## [Unreleased] - ReleaseDate
 
+- flakify
+
 ## [0.2.0] - 2024-04-23
 
 - improve README
---
+++ default.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  rustPlatform,
+}:
+
+rustPlatform.buildRustPackage {
+  pname = "pre-commit-sort";
+  version = "0.2.0";
+
+  src = lib.fileset.toSource {
+    root = ./.;
+    fileset = lib.fileset.unions [
+      ./Cargo.lock
+      ./Cargo.toml
+      ./src
+      ./tests
+    ];
+  };
+
+  cargoLock.lockFile = ./Cargo.lock;
+
+  meta = {
+    description = "Sort .pre-commit-config.yaml & .pre-commit-hooks.yaml";
+    homepage = "https://github.com/nim65s/pre-commit-sort";
+    changelog = "https://github.com/nim65s/pre-commit-sort/blob/main/CHANGELOG.md";
+    license = with lib.licenses; [
+      asl20
+      mit
+    ];
+    maintainers = with lib.maintainers; [ nim65s ];
+    mainProgram = "pre-commit-sort";
+  };
+}
